43abf76d9cb423d96dc882edea390a218d92b40e
Кристина Вишнякова <Lazurus_777@mail.ru>
2025-04-08 00:22:31 +0300

    end with gamestate

--- GameState.cs
+++ GameState.cs
@@ -87,7 +87,32 @@ namespace Tetris
 
         private void PlaceBlock()
         {
+            foreach(Position p in CurrentBlock.TilePositions())
+            {
+                GameGrid[p.Row, p.Column] = CurrentBlock.Id;
+            }
 
+            GameGrid.ClearFullRows();
+
+            if (IsGameOver())
+            {
+                GameOver = true;
+            }
+            else
+            {
+                CurrentBlock = BlockQueue.GetAndUpdate();
+            }
+        }
+
+        public void MoveBlockDown()
+        {
+            CurrentBlock.Move(1, 0);
+
+            if (!BlockFits())
+            {
+                CurrentBlock.Move(-1, 0);
+                PlaceBlock();
+            }
         }
     }
 }
